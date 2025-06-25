0a93d7cd7415ef060ed2b0b7eb317102c7aefa4d
rayyan2907 <mrayyan5296@gmail.com>
2025-06-25 18:00:43 +0500

    login page ui made

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:whatsapp_mobile/color.dart';
 import 'package:whatsapp_mobile/responsive/responsive_layout.dart';
 import 'package:whatsapp_mobile/screens/mobile_screen_layout.dart';
 import 'package:whatsapp_mobile/screens/web_screen_layout.dart';
+import 'package:whatsapp_mobile/widgets/mainPages/login.dart';
+
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+
+  Future<Widget> getHomeScreen() async{
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      final token = prefs.getString('jwt');
+      if (token != null && token.isNotEmpty) {
+        return const ResponsiveLayout(MobileScreenLayout: MobileScreenLayout(),
+            WebScreenLayout: WebScreenLayout());
+      }
+      else {
+        return const LoginScreen();
+      }
+    }
+    catch(e){
+      debugPrint("Error in getHomeScreen(): $e");
+      return const Scaffold(
+        body: Center(child: Text("Error loading screen")),
+      );
+    }
+  }
+
 
   // This widget is the root of your application.
   @override
@@ -21,7 +47,25 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor:  backgroundColor,
       ),
-      home: const ResponsiveLayout(MobileScreenLayout: MobileScreenLayout(),WebScreenLayout: WebScreenLayout()),
+      home: FutureBuilder(future: getHomeScreen(), builder: (context,snapshot){
+        if(snapshot.connectionState==ConnectionState.waiting){
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator(color: Colors.green,),),
+          );
+
+        }
+        else
+          {
+            if (snapshot.hasData && snapshot.data != null) {
+              return snapshot.data!;
+            } else {
+              return const Scaffold(
+                body: Center(child: Text("Something went wrong")),
+              );
+            }
+
+          }
+      })
     );
   }
 }
