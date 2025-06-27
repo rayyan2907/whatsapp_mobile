2b9917a1fdbb76161ba684990ef51ad258d88cd6
rayyan2907 <mrayyan5296@gmail.com>
2025-06-27 19:43:11 +0500

    message list ui improved

--- lib/widgets/messagesSection/chatList.dart
+++ lib/widgets/messagesSection/chatList.dart
@@ -14,43 +14,72 @@ class Chatlist extends StatefulWidget {
 }
 
 class _ChatlistState extends State<Chatlist> {
+  int offset = 0;
   List messages = [];
   bool _isLoading = false;
+  bool _isFetchingMore = false;
   final ScrollController _scrollController = ScrollController();
 
   @override
   void initState() {
     super.initState();
-    loadMessages();
+    loadMessages(); // initial load
+    _scrollController.addListener(_handleScroll);
   }
+
+  void _handleScroll() {
+    if (_scrollController.position.pixels >=
+        _scrollController.position.maxScrollExtent &&
+        !_isFetchingMore &&
+        !_isLoading) {
+      fetchMoreMessages(); // when top is reached (because reversed: true)
+    }
+  }
+
   void loadMessages() async {
     setState(() => _isLoading = true);
-
     final prefs = await SharedPreferences.getInstance();
     final token = prefs.getString('jwt');
 
-    final result = await Getmessages().loadMessages(
-      token,
-      widget.user['contact_id'],
-      0,
-    );
+    final result = await Getmessages()
+        .loadMessages(token, widget.user['user_id'], offset);
 
     if (!mounted) return;
 
     setState(() {
       _isLoading = false;
       messages = result ?? [];
+      offset += 15;
     });
 
     WidgetsBinding.instance.addPostFrameCallback((_) => scrollToBottom());
   }
 
+  void fetchMoreMessages() async {
+    setState(() => _isFetchingMore = true);
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('jwt');
+
+    final result = await Getmessages()
+        .loadMessages(token, widget.user['user_id'], offset);
+
+    if (!mounted) return;
+
+    if (result != null && result.isNotEmpty) {
+      setState(() {
+        messages.addAll(result); // append at the end (top visually)
+        offset += 15;
+      });
+    }
+
+    setState(() => _isFetchingMore = false);
+  }
 
   void scrollToBottom() {
     if (_scrollController.hasClients) {
       _scrollController.animateTo(
-        0, // because we use reverse: true
-        duration: const Duration(milliseconds: 300),
+        0,
+        duration: const Duration(milliseconds: 0),
         curve: Curves.easeOut,
       );
     }
@@ -64,36 +93,59 @@ class _ChatlistState extends State<Chatlist> {
       );
     }
 
-    return ListView.builder(
-      reverse: true, // Newest message at bottom
-      controller: _scrollController,
-      physics: const BouncingScrollPhysics(),
-      itemCount: messages.length,
-      itemBuilder: (context, index) {
-        final msg = messages[index];
-        return msg['is_sent'] == true
-            ? Sentmessage(
-          textMsg: msg['text_msg'].toString(),
-          date: msg['time'].toString(),
-          type: msg['type'].toString(),
-          imgUrl: msg['img_url'].toString(),
-          videoUrl: msg['video_url'].toString(),
-          caption: msg['caption'].toString(),
-          isSeen: msg['is_seen'] as bool? ?? false,
-          duration: msg['duration'].toString(),
-          voiceUrl: msg['voice_url'].toString(),
-        )
-            : ReceivedMessage(
-          textMsg: msg['text_msg'].toString(),
-          date: msg['time'].toString(),
-          type: msg['type'].toString(),
-          imgUrl: msg['img_url'].toString(),
-          videoUrl: msg['video_url'].toString(),
-          caption: msg['caption'].toString(),
-          duration: msg['duration'].toString(),
-          voiceUrl: msg['voice_url'].toString(),
-        );
-      },
+    return Stack(
+
+      children: [
+
+        ListView.builder(
+          reverse: true,
+          controller: _scrollController,
+          physics: const BouncingScrollPhysics(),
+          itemCount: messages.length + (_isFetchingMore ? 1 : 0),
+          itemBuilder: (context, index) {
+            if (_isFetchingMore && index == messages.length) {
+              return const Padding(
+                padding: EdgeInsets.symmetric(vertical: 15),
+                child: Center(
+                  child: CircularProgressIndicator(color: Colors.green),
+                ),
+              );
+            }
+
+            final msg = messages[index];
+            return msg['is_sent'] == true
+                ? Sentmessage(
+              textMsg: msg['text_msg'].toString(),
+              date: msg['time'].toString(),
+              type: msg['type'].toString(),
+              imgUrl: msg['img_url'].toString(),
+              videoUrl: msg['video_url'].toString(),
+              caption: msg['caption'].toString(),
+              isSeen: msg['is_seen'] as bool? ?? false,
+              duration: msg['duration'].toString(),
+              voiceUrl: msg['voice_url'].toString(),
+            )
+                : ReceivedMessage(
+              textMsg: msg['text_msg'].toString(),
+              date: msg['time'].toString(),
+              type: msg['type'].toString(),
+              imgUrl: msg['img_url'].toString(),
+              videoUrl: msg['video_url'].toString(),
+              caption: msg['caption'].toString(),
+              duration: msg['duration'].toString(),
+              voiceUrl: msg['voice_url'].toString(),
+            );
+          },
+        ),
+
+
+      ],
     );
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
   }
 }
